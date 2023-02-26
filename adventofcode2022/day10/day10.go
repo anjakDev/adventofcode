8bde41591524ffbe3a993f83575a0cf8bc886d23
Anja Klipic <anja.klipic@gmail.com>
2023-02-26 14:49:44 +0100

    solved day10 part 2

--- adventofcode2022/day10/day10.go
+++ adventofcode2022/day10/day10.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Part one: ", partone())
-	fmt.Println("Part two: ", parttwo())
+	parttwo()
 }
--- adventofcode2022/day10/day10part2.go
+++ adventofcode2022/day10/day10part2.go
@@ -1,5 +1,54 @@
 package main
 
-func parttwo() int {
-	return -1
+import (
+	"adventofcode/util"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func parttwo() {
+	fileScanner, closer := util.NewScanner("input-day10.txt")
+	defer closer.Close()
+
+	output := &strings.Builder{}
+
+	xRegister := 1
+	counter := 0
+	for fileScanner.Scan() {
+		// instruction can have two parts: "command value" where value is optional
+		instruction := strings.Split(fileScanner.Text(), " ")
+		if len(instruction) == 1 {
+			counter = drawSymbol(output, xRegister, counter)
+			continue
+		}
+		value, _ := strconv.Atoi(instruction[1])
+		counter = drawSymbol(output, xRegister, counter)
+		counter = drawSymbol(output, xRegister, counter)
+		xRegister += value
+	}
+	fmt.Printf(output.String())
+}
+
+func drawSymbol(output *strings.Builder, xRegister int, counter int) int {
+	var err error
+	if xRegister-1 <= counter && counter <= xRegister+1 {
+		_, err = output.WriteString("#")
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		_, err = output.WriteString(".")
+		if err != nil {
+			panic(err)
+		}
+	}
+	if counter == 39 {
+		_, err = output.WriteString("\n")
+		if err != nil {
+			panic(err)
+		}
+		return 0
+	}
+	return counter + 1
 }
